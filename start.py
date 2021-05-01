9992f6c130d343122b251c5a711c08fc1e3268f6
Unknown <panarellimarco99@gmail.com>
2021-05-01 16:28:12 +0200

    Added program execution time

--- start.py
+++ start.py
@@ -1,10 +1,17 @@
+import time
 from protocellule.proto import proto
 
 if __name__ == "__main__":
+
     file = input("Inserisci nome file(file.txt): ")
+
+    start_time = time.time()
+
     p = proto("conf.txt", file+".txt")
     # p = proto()
     p.simula()
+
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     p.print_to_file()
     p.print_graph()
