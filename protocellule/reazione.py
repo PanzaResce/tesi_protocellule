789056cf230a45a78dfabfa6e57bd8f76391b95a
panzaresce <marco.panarelli@protonmail.com>
2021-08-29 15:31:11 +0200

    Comments

--- protocellule/reazione.py
+++ protocellule/reazione.py
@@ -8,35 +8,35 @@ class reazione:
         self.prodotti = list()
         self.costante = float()
 
-        if self.tipo == 21:
+        if self.tipo == 21:     # creazione complesso
             self.reagenti = [reazione[0], reazione[1]]
             self.prodotti = [reazione[2]]
             self.costante = float(reazione[3])
-        elif self.tipo == 12:
+        elif self.tipo == 12:   # scissione complesso
             self.reagenti = [reazione[0]]
             self.prodotti = [reazione[1], reazione[2]]
             self.costante = float(reazione[3])
-        elif self.tipo == 22:
+        elif self.tipo == 22:   # scambio (complesso in prodotto)
             self.reagenti = [reazione[0], reazione[1]]
             self.prodotti = [reazione[2], reazione[3]]
             self.costante = float(reazione[4])
-        elif self.tipo == 10:
+        elif self.tipo == 10:   # scomparsa
             self.reagenti = [reazione[0]]
             self.prodotti = []
             self.costante = float(reazione[2])
-        elif self.tipo == 1:
+        elif self.tipo == 1:    # aggiunta
             self.reagenti = []
             self.prodotti = [reazione[1]]
             self.costante = float(reazione[2])
-        elif self.tipo == 23:
+        elif self.tipo == 23:   # cleavage diretto
             self.reagenti = [reazione[0], reazione[1]]
             self.prodotti = [reazione[2], reazione[3], reazione[4]]
             self.costante = float(reazione[5])
-        elif self.tipo == 32:
+        elif self.tipo == 32:   # cleavage inverso
             self.reagenti = [reazione[0], reazione[1], reazione[2]]
             self.prodotti = [reazione[3], reazione[4]]
             self.costante = float(reazione[5])
-        elif self.tipo == 210:
+        elif self.tipo == 210:  # passaggio di membrana
             self.reagenti = [reazione[0]]
             self.prodotti = [reazione[1]]
             self.costante = float(reazione[2])
